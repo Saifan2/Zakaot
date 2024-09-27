e8fa5be5a36c6ba80777cb123f0867efc5ddffb8
Saifan2 <122365013+Saifan2@users.noreply.github.com>
2024-09-27 14:20:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,51 @@ function generateBBCode() {
     let output = '';
     const honorAdmin = document.getElementById('honorAdmin').checked;
     const honorUser = document.getElementById('honorUser').checked;
+    const Supporter = document.getElementById('Supporter').checked;
+    const Tohen = document.getElementById('Tohen').checked;
+    const Mharhim = document.getElementById('Mharhim').checked;
+    const Hagah = document.getElementById('Hagah').checked;
+    const Tech = document.getElementById('Tech').checked;
+    const Kidom = document.getElementById('Kidom').checked;
+
 
     if (honorAdmin) {
-        output += '[COLOR=#FF7F50]מנהל כבוד[/COLOR]\n';
+        output += '[B][COLOR=#FF7F50]מנהל כבוד[/COLOR][/B]\n';
     }
 
     if (honorUser) {
-        output += '[COLOR=#FF8C00]משתמש כבוד[/COLOR]\n';
+        output += '[B][COLOR=#FF8C00]משתמש כבוד[/COLOR][/B]\n';
+    }
+    if (Supporter) {
+        output += '[B][COLOR=#0099FF]חבר בצוות תמיכה לשעבר[/COLOR][/B]\n';
+    }
+    if (Tohen) {
+        output += '[B][COLOR=#3366cc]חבר בצוות תוכן לשעבר[/COLOR][/B]\n';
+    }
+    if (Mharhim) {
+        output += '[B][COLOR=#999900]חבר בצוות מארחים לשעבר[/COLOR][/B]\n';
+    }
+    if (Hagah) {
+        output += '[B][COLOR=#33ccff]חבר בצוות הגהה לשעבר[/COLOR][/B]\n';
+    }
+    if (Tech) {
+        output += '[B][COLOR=#008b8b]חבר בצוות טכנולוגיה לשעבר[/COLOR][/B]\n';
+    }
+    if (Kidom) {
+        output += '[B][COLOR=#0099FF]חבר בצוות פיתוח וקידום לשעבר[/COLOR][/B]\n';
     }
 
     const managerInputs = document.querySelectorAll('.formerManagerInput');
     managerInputs.forEach(input => {
         if (input.value.trim() !== '') {
-            output += `[COLOR=#FF0000]מנהל פורום ${input.value.trim()} לשעבר[/COLOR]\n`;
+            output += `[B][COLOR=#FF0000]מנהל פורום ${input.value.trim()} לשעבר[/COLOR][/B]\n`;
         }
     });
 
     const supervisorInputs = document.querySelectorAll('.formerSupervisorInput');
     supervisorInputs.forEach(input => {
         if (input.value.trim() !== '') {
-            output += `[COLOR=#0099FF]מפקח קטגוריית ${input.value.trim()} לשעבר[/COLOR]\n`;
+            output += `[B][COLOR=#0099FF]מפקח קטגוריית ${input.value.trim()} לשעבר[/COLOR][/B]\n`;
         }
     });
 
